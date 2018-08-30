b045679345017a6179076a434f6c2345b82efe03
kanalumaddela <github@maddela.org>
2018-08-30 03:32:53 -0400

    keep 2nd placeholder for later usage

--- src/SteamLogin.php
+++ src/SteamLogin.php
@@ -149,7 +149,7 @@ class SteamLogin
         $this->apiKey = $this->options['api_key'];
         unset($this->options['api_key']);
 
-        self::$apiURL = sprintf(self::STEAM_API, $this->apiKey);
+        self::$apiURL = sprintf(self::STEAM_API, $this->apiKey, '%s');
 
         if ($this->options['session']['enable']) {
             if (session_status() == PHP_SESSION_NONE && !$this->options['session']['existing']) {
